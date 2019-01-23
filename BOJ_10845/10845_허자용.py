08dfa924b1e18ebfe6a22f4c90ef4116b45627cf
jygj65536 <41312810+jygj65536@users.noreply.github.com>
2019-01-23 09:05:53 +0900

    Create 10845_허자용.py

---
+++ BOJ_10845/10845_허자용.py
@@ -0,0 +1,50 @@
+class Node:
+    count = 0
+    def __init__(self, value):
+        self.value = value
+        Node.count += 1
+    def setprev(self, prev):
+        self.prev = prev
+    def setnext(self, next):
+        self.next = next
+
+
+T = int(input())
+
+for i in range(T):
+    text = input()
+    if text[:4] == "push":
+        pushvalue = int(text[5:])
+        if not(Node.count):
+            head = Node(pushvalue)
+            tail = head
+
+        else:
+            newest = Node(pushvalue)
+            newest.setprev(tail)
+            tail.setnext(newest)
+            tail = newest
+
+    elif text == "pop":
+        if not(Node.count):
+            print(-1)
+        else:
+            print(head.value)
+            Node.count -= 1
+            if Node.count:
+                head = head.next
+
+    elif text == "size":
+        print(Node.count)
+    elif text == "empty":
+        print(int(not((Node.count))))
+    elif text == "front":
+        if not(Node.count):
+            print(-1)
+        else:
+            print(head.value)
+    elif text == "back":
+        if not(Node.count):
+            print(-1)
+        else:
+            print(tail.value)
